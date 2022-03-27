6f4dd18f82421e7166dcf347c8f4787ca855e111
John <mur.sensi@yandex.ru>
2022-03-27 23:21:38 +0300

    ф
    ф

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 let root = document.getElementById("root");
 ReactDOM.render(<App  />, root);
 
-console.log(2)
+console.log(3)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
