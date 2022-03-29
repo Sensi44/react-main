1d27b73879ae0353f9a4822d72ff6f52cc6f60d5
Sensibleness <you@example.com>
2022-03-29 15:34:01 +0300

    New Add
    go to work

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React from "react";
-import TodoList from "./Todo/TodoList";
 
-function App() {
-  const todos = [
-    { id: 1, completed: false, title: "купить хлеб" },
-    { id: 2, completed: false, title: "купить масло" },
-    { id: 3, completed: false, title: "купить молоко" },
-  ];
 
-  return (
-    <div className="wrapper">
-      <h1>React tutorial</h1>
-      <h2>React tutorial2</h2>
-      <TodoList todos={todos} />
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default App;
--- src/math.js
+++
@@ -1,9 +0,0 @@
-import TodoList from "./Todo/TodoList";
-import React from "react";
-
-function App2() {
-  console.log(`test`);
-  return <div>test div</div>
-}
-
-export default App2;
