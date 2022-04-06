8f309c38c182516927a3a40981bff5345d0bff62
Sensibleness <you@example.com>
2022-04-06 17:05:11 +0300

    v 1.4
    let go

--- src/components/Footer/ItemStatusFilter/ItemStatusFilter.jsx
+++ src/components/Footer/ItemStatusFilter/ItemStatusFilter.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+
 import "./ItemStatusFilter.scss";
 
-const ItemStatusFilter = (props) => {
-  return (
-    <div className="footer-wrap">
-      <span className="todo-count">{props.count} items left</span>
+export default class ItemStatusFilter extends Component {
+  render() {
+    return (
+      <div className="footer-wrap">
+        <span className="todo-count">{this.props.count} items left</span>
 
-      <ul className="filters">
-        <li>
-          <button className="selected">All</button>
-        </li>
-        <li>
-          <button>Active</button>
-        </li>
-        <li>
-          <button>Completed</button>
-        </li>
-      </ul>
+        <ul className="filters">
+          <li>
+            <button className="selected">All</button>
+          </li>
+          <li>
+            <button>Active</button>
+          </li>
+          <li>
+            <button>Completed</button>
+          </li>
+        </ul>
 
-      <button className="clear-completed">Clear completed</button>
-    </div>
-  );
-};
+        <button className="clear-completed">Clear completed</button>
+      </div>
+    );
+  }
+}
 
-export default ItemStatusFilter;
+//
+// const ItemStatusFilter = (props) => {
+//   return (
+//     <div className="footer-wrap">
+//       <span className="todo-count">{props.count} items left</span>
+//
+//       <ul className="filters">
+//         <li>
+//           <button className="selected">All</button>
+//         </li>
+//         <li>
+//           <button>Active</button>
+//         </li>
+//         <li>
+//           <button>Completed</button>
+//         </li>
+//       </ul>
+//
+//       <button className="clear-completed">Clear completed</button>
+//     </div>
+//   );
+// };
+//
+// export default ItemStatusFilter;
