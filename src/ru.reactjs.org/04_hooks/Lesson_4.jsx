597c34d987e939a10380631bae1bdece47d7d814
John <mur.sensi@yandex.ru>
2022-04-14 21:01:52 +0300

    a
    a

--- src/ru.reactjs.org/04_hooks/Lesson_4.jsx
+++ src/ru.reactjs.org/04_hooks/Lesson_4.jsx
@@ -1,6 +1,7 @@
-import React, {Component, useState, useEffect, useRef, useMemo, Fragment} from "react";
+import React, {useState, useEffect, useRef, useMemo, Fragment} from "react";
 import Count from "./Count/Count";
 import IsFive from "./IsFive/IsFive";
+
 
 import "./Lesson_4.scss"
 
@@ -8,6 +9,32 @@ import "./Lesson_4.scss"
 // useMemo - указывает какую функцию мемоизировать
 // Следит за выполнением функции, по зависимостям отслеживания.
 // Если зависимости не меняются, возвращает предыдущий результат функции
+
+
+// function Lesson_4() {
+//   const [numbers, setNumbers] = React.useState([1,2,3,4,5,6,7]);
+//   const ulRef =  React.useRef();
+//   const timerRef =  React.useRef();
+//
+//   const addNumber = () => {
+//     setNumbers((prev) =>{
+//       console.log(prev)
+//       return [...prev, prev[prev.length - 1] + 1]
+//     })
+//   }
+//
+//   const start = () => {
+//     timerRef.current = setInterval(addNumber, 1000);
+//   }
+//
+//   const stop = () => {
+//     console.log(timerRef.current)
+//     clearInterval(timerRef.current);
+//   }
+//
+//   const handleScroll = () => {
+//     console.log('Был скролл')
+//   }
 
 
 function Lesson_4() {
@@ -32,7 +59,7 @@ function Lesson_4() {
   )
 }
 
-export default Lesson_4
+export default Lesson_4;
 
 
 
